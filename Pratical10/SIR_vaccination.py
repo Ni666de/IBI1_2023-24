d731fa7eac8ea276b74bffd98514fc509bc2f74e
Jiayi Wang 16sui <156885975+Ni666de@users.noreply.github.com>
2024-05-21 02:43:00 +0800

    Create SIR_vaccination.py

---
+++ Pratical10/SIR_vaccination.py
@@ -0,0 +1,34 @@
+mport numpy as np
+import matplotlib.pyplot as plt
+
+N = 10000  
+beta = 0.3  
+gamma = 0.05  
+S, I, R = N - 1, 1, 0  
+S_list, I_list, R_list = [], [], []
+
+time_steps = 1000
+for _ in range(time_steps):
+    new_infections = np.random.binomial(S, I / N)
+    new_recoveries = np.random.binomial(I, gamma)
+    
+    S -= new_infections
+    I += new_infections - new_recoveries
+    R += new_recoveries
+    
+    S_list.append(S)
+    I_list.append(I)
+    R_list.append(R)
+
+plt.plot(S_list, label='Susceptible')
+plt.plot(I_list, label='Infected')
+plt.plot(R_list, label='Recovered')
+plt.xlabel('Time')
+plt.ylabel('Number of People')
+plt.title('SIR Model')
+plt.legend()
+plt.show()
+vaccinated_rate = 0.1
+S = N * (1 - vaccinated_rate) - 1
+I = 1
+R = 0

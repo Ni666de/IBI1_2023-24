301ed1e6dfc3c73dfcc1e46be49fcb9d849164ae
Jiayi Wang 16sui <156885975+Ni666de@users.noreply.github.com>
2024-05-20 19:40:26 +0800

    Create  File dalys.py

---
+++ Pratical7/File dalys.py
@@ -0,0 +1,43 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+os.chdir("/path/to/your/directory")
+data_file = "dalys-rate-from-all-causes.csv"
+dalys_data = pd.read_csv(data_file)
+
+print(dalys_data.head())
+
+print(dalys_data.info())
+
+print(dalys_data.describe())
+
+print(dalys_data.iloc[::10, 3])
+
+afghanistan_data = dalys_data[dalys_data['Entity'] == 'Afghanistan']
+print(afghanistan_data['DALYs'])
+
+china_data = dalys_data[dalys_data['Entity'] == 'China']
+
+mean_dalys_china = china_data['DALYs'].mean()
+print(f"Mean DALYs in China: {mean_dalys_china}")
+print(f"DALYs in China in 2019: {china_data.loc[china_data['Year'] == 2019, 'DALYs'].values[0]}")
+is_above_mean = china_data.loc[china_data['Year'] == 2019, 'DALYs'] > mean_dalys_china
+print(f"Was 2019 above or below the mean DALYs in China? {'Above' if is_above_mean else 'Below'}")
+
+plt.plot(china_data['Year'], china_data['DALYs'], 'b+')
+plt.xlabel('Year')
+plt.ylabel('DALYs')
+plt.title('DALYs in China Over Time')
+plt.xticks(rotation=-90) 
+plt.show()
+
+question = "How has the DALYs changed in the UK over time?"
+question_file = "question.txt"
+
+
+with open(question_file, "w") as f:
+    f.write(f"Question: {question}\n")
+    f.write("Line number in dalys.py: 99\n")
+    f.write("The result is...\n")

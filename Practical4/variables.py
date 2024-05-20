9cc5d0b032046ad6cc5290393d3e3b535740ea30
Jiayi Wang 16sui <156885975+Ni666de@users.noreply.github.com>
2024-05-20 18:48:01 +0800

    Update variables.py

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -1,1 +1,20 @@
+a = 40  
+b = 36  
+c = 30  
+
+d = a - b  
+e = b - c  
+
+
+so e>d,train using a combination of running and strength exercises is more effective
+
+X = True
+Y = False
+
+W = X or Y  
+X=T, Y=T, W=T
+X=T, Y=F, W=T
+X=F, Y=T, W=T
+X=F, Y=F, W=F
+
 
